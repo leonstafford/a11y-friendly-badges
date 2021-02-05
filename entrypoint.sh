1d8223d2e662b90f4b03c886980b30c4d7204d44
Leon Stafford <me@ljs.dev>
2021-02-06 09:38:45 +1000

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,18 @@ set -eou pipefail
 # Debug: just testing output while building
 echo "### DEBUG ###"
 
+pwd
+
+ls
+
+ls /home/runner/work/WhatWouldViktorDo/WhatWouldViktorDo
+
+ls /home/runner/work/WhatWouldViktorDo
+
+
+
+
+
 # run the user supplied function
 execute_user_command() {
   set -x
